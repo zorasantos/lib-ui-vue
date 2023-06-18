86613ac089c3793011dd5f6de7b2015b7431952a
zorasantos <zoranildo22@gmail.com>
2023-06-18 14:49:46 -0300

    build: new config build

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,15 @@
 
 import { defineConfig } from "vite";
 import { resolve } from "path";
+import { terser } from "rollup-plugin-terser";
+import postcss from "rollup-plugin-postcss";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), dts()],
+  plugins: [vue(), dts(), terser(), postcss()],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
